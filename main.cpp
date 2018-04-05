04dd78692e5111334a310ba6e5b7d451a6256139
Александр Дабагян <DabagyanAE@mpei.ru>
2018-04-05 08:55:55 +0300

    Третий

--- main.cpp
+++ main.cpp
@@ -1,6 +1,11 @@
 #include <iostream>
 
-int main() {
-    std::cout << "Hello, World!" << std::endl;
+using namespace std;
+
+int main ()
+{
+    int a, b;
+    cout << "Enter A & B: \n\n";
+    cin >> a >> b;
     return 0;
 }
